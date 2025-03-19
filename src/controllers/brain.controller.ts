eb709df795a5e347f53aa7746386fa44053fee65
Aryan Bhat <aryanbhat2527@gmail.com>
2025-03-20 02:04:46 +0530

    shareable link added

--- src/controllers/brain.controller.ts
+++ src/controllers/brain.controller.ts
@@ -10,12 +10,19 @@ const brainShare = asyncHandler(async (req: Request, res: Response): Promise<voi
         throw new Error('Trouble getting user id, please try again');
     }
     const response = await brainService.shareBrain(userId, share);
-
-    res.status(200).json({
-        status: 'success',
-        message: 'Updated shareable link',
-        response: response || '',
-    });
+    if (response) {
+        res.status(200).json({
+            status: 'success',
+            message: 'Updated shareable link',
+            response: `http://localhost:3001/api/v1/brain/${response}`,
+        });
+    }
+    else {
+        res.status(200).json({
+            status: 'success',
+            message: 'Deleted shareable link',
+        });
+    }
 });
 const getBrain = asyncHandler(async (req: Request, res: Response): Promise<void> => {
     const hash = req.params.shareLink;
